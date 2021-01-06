397026ef71b0357a6122fde46efd9c9a16e6f7f5
“aykutsoyuk” <aykutsoyuk@gmail.com>
2021-01-06 15:50:31 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import About from './Components/About'
+import Comments from './Components/Comments'
+import Work from './Components/Work'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+      <div className="App">
+      <div className="navigation">
+          <div className="navigation-sub">
+            <Link to="/" className="item">Work</Link>
+            <Link to="/comments" className="item">Comments</Link>
+            <Link to="/about" className="item">About</Link>
+          </div>
+        </div>
+        <Route exact path="/" component={Work} />
+        <Route path="/comments" component={Comments} />
+        <Route path="/about" component={About} />
+      </div>
+    </BrowserRouter>
   );
 }
 
